89c7fb506db9dbe4e3073a3eb6b52d974fd9e9bf
Oidhche <julia_voigt.1@gmx.de>
2018-04-26 10:54:28 +0200

    Update hdhdhd

--- MainClass.cs
+++ MainClass.cs
@@ -13,6 +13,9 @@ namespace StartingPoint
 		[STAThread]
 		static void Main(string[] args)
 		{
+
+
+            Console.WriteLine("Hello");
 			// Create movies
 			Movie movCinderella = new Movie("Cinderella", PriceCodes.Childrens);
 			Movie movStarWars = new Movie("Star Wars", PriceCodes.Regular);
